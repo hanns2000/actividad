67d3007d4fd130031713f2cd4394e8f610e0effa
hanns2000 <130723155+hanns2000@users.noreply.github.com>
2023-07-12 00:33:02 -0500

    Add files via upload

---
+++ prc2.js
@@ -0,0 +1,11 @@
+ScrollReveal ({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 300
+});
+
+ScrollReveal().reveal('.text-header, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img', { origin: 'buttom' })
+ScrollReveal().reveal('.text-header h1', { origin: 'left' });
+ScrollReveal().reveal('.text-header p', { origin: 'rigth' });
